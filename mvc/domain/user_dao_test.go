f98759f6aa34c3c56a0a47865fda1a89c6cb9e64
Engin Bulut <engin@enginbulut.com.tr>
2020-10-13 02:09:05 +0300

    bug fix

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -5,12 +5,11 @@ import (
 	"net/http"
 	"testing"
 )
-
 func TestGetUserNoUserFound(t *testing.T) {
 	// Initialization:
 
 	// Execution:
-	user, err := GetUser(0)
+	user, err := UserDao.GetUser(0)
 
 	// Validation:
 	assert.Nil(t, user, "we were not expecting a user with id 0")
@@ -18,17 +17,17 @@ func TestGetUserNoUserFound(t *testing.T) {
 
 	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
 	assert.EqualValues(t, "not_found", err.Code)
-	assert.EqualValues(t, "user 0 was not found\n", err.Message)
+	assert.EqualValues(t, "user 0 does not exists", err.Message)
 }
 
 func TestGetUserNoError(t *testing.T) {
-	user, err := GetUser(123)
+	user, err := UserDao.GetUser(123)
 
 	assert.Nil(t, err)
 	assert.NotNil(t, user)
 
 	assert.EqualValues(t, 123, user.Id)
-	assert.EqualValues(t, "Engin", user.FirstName)
-	assert.EqualValues(t, "Bulut", user.LastName)
-	assert.EqualValues(t, "test@email.com", user.Email)
+	assert.EqualValues(t, "Fede", user.FirstName)
+	assert.EqualValues(t, "Leon", user.LastName)
+	assert.EqualValues(t, "myemail@gmail.com", user.Email)
 }
